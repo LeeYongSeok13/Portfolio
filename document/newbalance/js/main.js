a0ff9caba7f49ca564478d6ee7e191f487974a87
LeeYongSeok13 <minamiharuka333@gmail.com>
2022-09-16 17:46:41 +0900

    20220916

---
+++ document/newbalance/js/main.js
@@ -0,0 +1,53 @@
+$(function () {
+    $('#header_menu .menu_button i').on('click', function () {
+        $('#header_menu .gnb').toggleClass('on');
+    });
+
+    $('#ad_banner i').on('click', function () {
+        $('#ad_banner').slideUp();
+    });
+
+    $('.main_slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+
+    $('#main_visual .angle i:nth-child(1)').on('click', function () {
+        $('.main_slider').slick('slickPrev');
+    });
+    $('#main_visual .angle i:nth-child(2)').on('click', function () {
+        $('.main_slider').slick('slickNext');
+    })
+
+    $('#model_information .controls i:nth-child(1)').on('click', function () {
+        $('.iu').trigger('pause');
+    });
+    $('#model_information .controls i:nth-child(2)').on('click', function () {
+        $('.iu').trigger('play');
+    })
+
+    $('.shirt_slider').slick({
+        slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false,
+    });
+
+    $('.xi-arrow-left').on('click', function () {
+        $('.shirt_slider').slick('slickPrev');
+    });
+    $('.xi-arrow-right').on('click', function () {
+        $('.shirt_slider').slick('slickNext');
+    });
+
+    $('.tab_menu figure').on('click', function (event) {
+        event.preventDefault();
+        var idx = $(this).index();
+        $('.tab_content>div').removeClass('on');
+        $('.tab_content>div').eq(idx).addClass('on');
+    });
+})
---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(function () {
+    $('#Main').fullpage({
+        anchors: ['Introduce', 'Portfolio', 'End'],
+    })
+});
