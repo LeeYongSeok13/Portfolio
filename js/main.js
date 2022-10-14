fc3169c7960bf720852c24b7fc343831731759ca
LeeYongSeok13 <minamiharuka333@gmail.com>
2022-10-14 17:47:53 +0900

    20221014

--- js/main.js
+++ js/main.js
@@ -1,22 +1,26 @@
 $(function () {
 
     $('#Fullpage').fullpage({
-        sectionsColor: ['#57648C', '#6C7A89', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff'],
+        sectionsColor: ['#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff',],
         navigation: true,
         loopTop: true,
         loopBottom: true,
         afterLoad: function (anchorLink, index) {
             if (index == 1) {
                 $('.section01 .container .left').addClass('on');
-                $('.section02 .dis .left').addClass('on');
+                $('.section01 .container .right').addClass('on');
             }
         },
         onLeave: function (index, nextIndex, direction) {
             if (index == 1) {
                 $('.section01 .container .left').removeClass('on');
-                $('.section02 .dis .left').removeClass('on');
+                $('.section01 .container .right').removeClass('on');
             }
         }
+    });
+
+    $('#footer a').on('click', function (event) {
+        $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top }, 0);
     });
 
 })
