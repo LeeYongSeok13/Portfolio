5be9e027f9304e625797cbeec5833970910cb3f0
LeeYongSeok13 <minamiharuka333@gmail.com>
2022-10-20 11:25:37 +0900

    20221020

--- document/drama/js/main.js
+++ document/drama/js/main.js
@@ -1,45 +1,22 @@
-$(function () {
-    $('.wheel_icon a').on('click', function (e) {
-        e.preventDefault();
-        var hash = $(this.hash).offset().top;
-        $('html,body').animate({ scrollTop: hash }, 3000)
-    });
+window.addEventListener('DOMContentLoaded', function () {
 
-    $('.product_slider').slick({
-        arrows: false,
-        autoplay: true,
-    });
-
-    $('.beer_slider').on('init afterChange', function () {
-        var current = $('.slick-current');
-        current.addClass('on').siblings().removeClass('on');
-    });
-
-    $('.beer_slider').slick({
-        arrows: false,
-        autoplay: true,
-    });
-
-    $(window).on('scroll', function () {
-        var box = $(window).scrollTop();
-        if (box > 300) {
-            $('.event .box').fadeIn(300);
-        } else {
-            $('.event .box').fadeOut(1000);
-        }
+    window.addEventListener('scroll', function () {
+        let sct = window.scrollY;
+        sct > 0
+            ? this.document.querySelector('.Header').classList.add('on')
+            : this.document.querySelector('.Header').classList.remove('on')
     })
-
-    $('.toTop').on('click', function () {
-        $('html,body').animate({ scrollTop: 0 }, 200);
-    });
-
-    $(window).on('scroll', function () {
-        var sct = $(window).scrollTop();
-        if (sct > 500) {
-            $('.toTop').fadeIn(300);
-        } else {
-            $('.toTop').fadeOut(1000);
-        }
+    const rightslider = new Swiper('.right_slider', {
+        loop: true,
+        effect: 'fade',
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
     });
 
 })
